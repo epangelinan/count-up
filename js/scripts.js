97b56568fb98be134f0963a8a7d7462dfffea60c
Esvethlynna Pangelinan and Joel Bakken <esvethlynna@>
2017-06-06 09:51:57 -0700

    add code to execute for loop ifcountBy is less than countTo

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var countTo = parseInt(prompt("Enter a number that you want to count up to:"));
   var countBy = parseInt(prompt("Enter another number that you want to count by:"));
 
-if ((countTo >= 0) && (countBy >= 0) && (typeof countTo === "number") && (typeof countBy === "number")) {
+if ((countTo >= 0) && (countBy >= 0) && (typeof countTo === "number") && (typeof countBy === "number") && (countBy < countTo)) {
   for (var index = 0; index <= countTo; index += countBy) {
     alert(index);
   }
